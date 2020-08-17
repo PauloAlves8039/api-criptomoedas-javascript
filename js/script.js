37cdeddb316d6bfac8970de65e2ad37254bb31e7
PauloAlves8039 <pj38alves@gmail.com>
2020-08-17 18:21:43 -0300

    Adicionando lógica para exibição dos dados

--- js/script.js
+++ js/script.js
@@ -4,3 +4,39 @@
  * @description: responsável pela integração com a API do CoinMarketCap para exibição de criptomoedas.
  * @version 1.0.0 (17/08/2020)
  */
+
+let apikey = {
+  key: "DIGITE SUA API KEY AQUI",
+};
+
+fetch(
+  "https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?CMC_PRO_API_KEY=" + apikey.key
+)
+  .then((response) => {
+    if (!response.ok)
+      throw new Error(
+        "Erro ao executar a requisição, status " + response.status
+      );
+    return response.json();
+  })
+  .then((api) => {
+    var texto = "";
+    for (let i = 0; i < 10; i++) {
+      texto =
+        texto +
+        `
+        <div class="media">
+        <img src="assets/coin.jpg" class="align-self-center mr-3" alt="coin" width="100" height="60">
+        <div class="media-body">
+        <h5 class="mt-2">${api.data[i].name}</h5>
+        <p>${api.data[i].symbol}</p>
+        </div>
+    </div>
+    `;
+    document.getElementById("coins").innerHTML = texto;
+    console.log(api.data[i].name);
+    }
+  })
+  .catch((error) => {
+    console.error(error.message);
+  });
