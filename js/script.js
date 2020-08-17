7db3617b19d73e87ab0b058777541e1feb4dfa22
PauloAlves8039 <pj38alves@gmail.com>
2020-08-17 17:28:13 -0300

    Versão inicial da aplicação

---
+++ js/script.js
@@ -0,0 +1,6 @@
+/**
+ * @file: script.js
+ * @author: Paulo Alves
+ * @description: responsável pela integração com a API do CoinMarketCap para exibição de criptomoedas.
+ * @version 1.0.0 (17/08/2020)
+ */
